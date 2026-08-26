0d8bff69bb641f60d071ce682efae7191420e86a
Ssnibles <joshua.breite@gmail.com>
2026-08-26 20:49:20 +1200

    Extract non trivial parts

--- util.h
+++ util.h
@@ -1,5 +1,11 @@
 /* See LICENSE.dwm file for copyright and license details. */
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stddef.h>
 
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
 int fd_set_nonblock(int fd);
+
+#endif /* UTIL_H */
