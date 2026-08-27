7f87b37327a81c51e60c245aad2bd6dd93a824e4
Ssnibles <joshua.breite@gmail.com>
2026-08-28 11:14:00 +1200

    fix(binds): Match layout binds to list order

--- include/config.h
+++ include/config.h
@@ -221,10 +221,10 @@ static const Key keys[] = {
 
 	/* Layout Controls */
 	{ MODKEY,                    XKB_KEY_o,           toggleoverview,   {0} },
-	{ MODKEY,                    XKB_KEY_t,           setlayout,        {.v = &layouts[0]} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_t,           setlayout,        {.v = &layouts[1]} },
-	{ MODKEY,                    XKB_KEY_m,           setlayout,        {.v = &layouts[3]} },
-	{ MODKEY,                    XKB_KEY_r,           setlayout,        {.v = &layouts[4]} },
+  { MODKEY,                    XKB_KEY_r,           setlayout,        {.v = &layouts[0]} },
+	{ MODKEY,                    XKB_KEY_t,           setlayout,        {.v = &layouts[1]} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_t,           setlayout,        {.v = &layouts[2]} },
+	{ MODKEY,                    XKB_KEY_m,           setlayout,        {.v = &layouts[4]} },
 	{ MODKEY,                    XKB_KEY_s,           setlayout,        {.v = &layouts[5]} },
 
 	/* Screenshots */
